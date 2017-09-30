4a1e23a5eb4f80ab7917ab71a280bdb96e5ba5b9
Zach <zachgraves512@gmail.com>
2017-09-29 22:23:40 -0400

    Setup of Main Activity. Started working on AsyncTask Class and button functionality for Main activity.

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.trivia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start, exit;
+    TextView loading;
+    ImageView triviaImag;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.buttonStartTrivia);
+        start.setActivated(false);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        exit = (Button) findViewById(R.id.buttonExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/trivia/TaskClass.java
@@ -0,0 +1,16 @@
+package com.example.trivia;
+
+import android.os.AsyncTask;
+
+
+
+public class TaskClass extends AsyncTask<String, Void, String> {
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+
+        return null;
+    }
+}
